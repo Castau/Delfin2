03422b4b773b0b4f3035565086c15b5381c0dd33
HrBjarup <asger@bjarup.dk>
2018-11-13 10:32:50 +0100

    Adding tests

---
+++ Delfin/test/domainlogic/MemberCompetitiveTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domainlogic;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Shevitar
+ */
+public class MemberCompetitiveTest {
+    
+    public MemberCompetitiveTest() {
+    }
+    
+}
